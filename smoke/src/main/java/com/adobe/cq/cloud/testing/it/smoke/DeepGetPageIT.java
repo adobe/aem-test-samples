2686c1e0c99c61de2a90db0f973ab18bd48c5bbd
Alexei Krainiouk <akrainio@adobe.com>
2019-09-20 09:35:42 -0700

    Improve smoke tests to check related resources.

---
+++ smoke/src/main/java/com/adobe/cq/cloud/testing/it/smoke/DeepGetPageIT.java
@@ -0,0 +1,146 @@
+/*
+ * Copyright 2018 Adobe Systems Incorporated
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.adobe.cq.cloud.testing.it.smoke;
+
+import org.apache.sling.testing.clients.ClientException;
+import org.apache.sling.testing.clients.SlingHttpResponse;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import com.adobe.cq.testing.junit.rules.CQAuthorPublishClassRule;
+import com.adobe.cq.testing.junit.rules.CQRule;
+import com.adobe.cq.testing.junit.rules.Page;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.ClassRule;
+import org.junit.Ignore;
+import org.junit.Rule;
+import org.junit.Test;
+
+import static org.apache.commons.io.IOUtils.closeQuietly;
+import static org.junit.Assert.assertEquals;
+
+public class DeepGetPageIT {
+
+    @ClassRule
+    public static CQAuthorPublishClassRule cqBaseClassRule = new CQAuthorPublishClassRule();
+
+    @Rule
+    public CQRule cqBaseRule = new CQRule(cqBaseClassRule.authorRule, cqBaseClassRule.publishRule);
+
+    @Rule
+    public Page root = new Page(cqBaseClassRule.authorRule);
+
+    private static HtmlUnitClient adminAuthor;
+    private static HtmlUnitClient adminPublish;
+
+    @BeforeClass
+    public static void beforeClass() {
+        adminAuthor = cqBaseClassRule.authorRule.getAdminClient(HtmlUnitClient.class);
+        adminPublish = cqBaseClassRule.publishRule.getAdminClient(HtmlUnitClient.class);
+    }
+
+    @AfterClass
+    public static void afterClass() throws IOException {
+        closeQuietly(adminAuthor);
+        closeQuietly(adminPublish);
+    }
+    
+    /**
+     * Verifies that the homepage exists on author
+     */
+    @Test
+    public void testHomePageAuthor() throws ClientException, IOException, URISyntaxException {
+        verifyPageAndResources(adminAuthor, "/");
+    }
+
+    /**
+     * Verifies that the sites console exists on author
+     */
+    @Test
+    public void testSitesAuthor() throws ClientException, IOException, URISyntaxException {
+        verifyPageAndResources(adminAuthor, "/sites.html");
+    }
+
+    /**
+     * Verifies that the assets console exists on author
+     */
+    @Test
+    public void testAssetsAuthor() throws ClientException, IOException, URISyntaxException {
+        verifyPageAndResources(adminAuthor, "/assets.html");
+    }
+
+    /**
+     * Verifies that the projects console exists on author
+     */
+    @Test
+    public void testProjectsAuthor() throws ClientException, IOException, URISyntaxException {
+        verifyPageAndResources(adminAuthor, "/projects.html");
+    }
+
+    /**
+     * Verifies that the homepage exists on publish
+     */
+    @Test @Ignore
+    public void testHomePagePublish() throws ClientException, IOException, URISyntaxException {
+        verifyPageAndResources(adminPublish, "/");
+    }
+
+    //*********************************************
+    // Internals
+    //*********************************************
+
+    /**
+     * Verifies that specified page as well as the resources reference in it are available.
+     * <P>
+     * <b>Limitation:</b> Due to issues in HtmlUnit library javascript execution is disabled and as a result
+     * only statically referenced resources are checked. All dynamically generated components
+     * of the page are thus ignored.
+     * @param client CQClient instance to use for accessing AEM.
+     * @param path path to the tested page.
+     * @throws ClientException
+     * @throws IOException
+     * @throws URISyntaxException
+     */
+    private static void verifyPageAndResources(HtmlUnitClient client, String path) throws ClientException, IOException, URISyntaxException {
+        URI baseURI = client.getUrl();
+        for (URI ref : client.getResourceRefs(path)) {
+            if (isSameOrigin(baseURI, ref)) {
+                SlingHttpResponse response = client.doGet(ref.getPath());
+                int statusCode = response.getStatusLine().getStatusCode();
+                assertEquals("Unexpected status returned from [" + ref + "]", 200, statusCode);
+            }
+        }
+    }
+
+    /** Checks if two URIs have the same origin
+     * @param uri1 first URI
+     * @param uri2 second URI
+     * @return true if two URI come from the same host, port and use the same scheme
+     */
+    private static boolean isSameOrigin(URI uri1, URI uri2) {
+        if (!uri1.getScheme().equals(uri2.getScheme())) {
+            return false;
+        } else if (!uri1.getAuthority().equals(uri2.getAuthority())) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+}
---
+++ smoke/src/main/java/com/adobe/cq/cloud/testing/it/smoke/HtmlUnitClient.java
@@ -0,0 +1,168 @@
+/*
+ * Copyright 2018 Adobe Systems Incorporated
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.adobe.cq.cloud.testing.it.smoke;
+
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.Credentials;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.impl.client.BasicCredentialsProvider;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.sling.testing.clients.ClientException;
+import org.apache.sling.testing.clients.SlingClientConfig;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import com.adobe.cq.testing.client.CQClient;
+import com.gargoylesoftware.htmlunit.DefaultCredentialsProvider;
+import com.gargoylesoftware.htmlunit.DefaultCssErrorHandler;
+import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.WebClientOptions;
+import com.gargoylesoftware.htmlunit.WebRequest;
+import com.gargoylesoftware.htmlunit.WebResponse;
+import com.gargoylesoftware.htmlunit.html.DomNode;
+import com.gargoylesoftware.htmlunit.html.HtmlPage;
+import org.w3c.dom.Node;
+
+/**
+ * AEM client that maintains a WebClient instance from HttpUnit framework
+ */
+public class HtmlUnitClient extends CQClient {
+
+    private final WebClient webClient = new WebClient();
+
+    /** Extracts references to external resources used by the specified page.
+     * This method extracts references from script, img, meta and link tags.
+     * @param path path to the page.
+     * @return list of URIs resolved against the pages baseURL
+     * @throws IOException when IO error occurs
+     * @throws URISyntaxException if malformed URL reference is found.
+     */
+    public List<URI> getResourceRefs(String path) throws IOException, URISyntaxException {
+        HtmlPage page = getPage(path, false);
+        List<URI> result = new ArrayList<>();
+        result.addAll(getRefs(page, "script", "src"));
+        result.addAll(getRefs(page, "img", "src"));
+        result.addAll(getRefs(page, "meta", "href"));
+        result.addAll(getRefs(page, "link", "href"));
+        return result;
+    }
+
+    /**
+     * Loads html page specified by path.
+     * @param path path to the page
+     * @param javaScriptEnabled whether to execute javascript
+     * @return parsed page.
+     * @throws IOException if IO error occurs.
+     */
+    public HtmlPage getPage(String path, boolean javaScriptEnabled) throws IOException {
+        WebClientOptions options = webClient.getOptions();
+        boolean wasJsEnabled = options.isJavaScriptEnabled();
+        try {
+            options.setJavaScriptEnabled(javaScriptEnabled);
+            return getPage(webClient, getUrl(path).toURL());
+        } finally {
+            options.setJavaScriptEnabled(wasJsEnabled);
+        }
+    }
+
+    //*********************************************
+    // Creation
+    //*********************************************
+
+    public HtmlUnitClient(CloseableHttpClient http, SlingClientConfig config) throws ClientException {
+        super(http, config);
+        URI baseUri = getUrl();
+        AuthScope scope = new AuthScope(baseUri.getHost(), baseUri.getPort());
+        DefaultCredentialsProvider credProvider = new DefaultCredentialsProvider();
+        credProvider.addCredentials(getUser(), getPassword());
+        Credentials creds = new UsernamePasswordCredentials(getUser(), getPassword());
+        BasicCredentialsProvider credentialsProvider = new BasicCredentialsProvider();
+        credentialsProvider.setCredentials(scope, creds);
+        webClient.setCredentialsProvider(credentialsProvider);
+        login(webClient);
+    }
+
+    @Override
+    public void close() throws IOException {
+        try {
+            webClient.close();
+        } finally {
+            super.close();
+        }
+    }
+
+    //*********************************************
+    // Internals
+    //*********************************************
+
+    private static List<URI> getRefs(HtmlPage page, String tag, String refAttr) throws URISyntaxException {
+        URI baseUri = new URI(page.getBaseURI());
+        List<URI> result = new ArrayList<>();
+        for (DomNode child : page.getElementsByTagName(tag)) {
+            Node src = child.getAttributes().getNamedItem(refAttr);
+            if (src != null) {
+                String href = src.getNodeValue();
+                URI uri = baseUri.resolve(href);
+                result.add(uri);
+            }
+        }
+        return result;
+    }
+
+    /**
+     *  Loads requested page while suppressing CSS errors (logged as warnings)
+     * @param webClient web client to use for loading
+     * @param url page URL
+     * @return loaded HtmlPage instance.
+     * @throws IOException when error occurs
+     */
+    private static HtmlPage getPage(WebClient webClient, URL url) throws IOException {
+        Logger logger = Logger.getLogger(DefaultCssErrorHandler.class.getName());
+        Level originalLevel = logger.getLevel();
+        try {
+            logger.setLevel(Level.SEVERE);
+            return webClient.getPage(url);
+        } finally {
+            logger.setLevel(originalLevel);
+        }
+    }
+
+    /**
+     * Preemptively logins to the server
+     * @param webClient web client to use for logging in.
+     * @throws ClientException
+     */
+    private void login(WebClient webClient) throws ClientException {
+        URL loginUrl = null;
+        try {
+            loginUrl = getUrl("/bin/receive?sling:authRequestLogin=1").toURL();
+            WebResponse response = webClient.loadWebResponse(new WebRequest(loginUrl));
+            if (response.getStatusCode() != 200) {
+                throw new ClientException("Unable to login to server: [" + loginUrl + "]. Unexpected status: " + response.getStatusCode());
+            }
+        } catch (IOException e) {
+            throw new ClientException("Unable to login to server: [" + loginUrl + "]", e);
+        }
+    }
+
+}
