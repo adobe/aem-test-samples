d2d7561fa1b38650faa4f59e47ed585127e8f1a2
Antonel Dobre <adobre@adobe.com>
2019-10-17 19:43:45 +0300

    [CQ-4280753] Add Sites Admin sanity tests in ESP
    Adding Create and Delete File test to
    AssetsActionIT.
    
    Renamed AssetsActionIT to AssetsAndFilesActionIT.
    
    Signed-off-by: Antonel Dobre <adobre@adobe.com>

--- ui-wcm-admin-smoke/src/main/java/com/adobe/cq/cloud/testing/it/ui/wcm/admin/smoke/AssetsAndFilesActionIT.java
+++ ui-wcm-admin-smoke/src/main/java/com/adobe/cq/cloud/testing/it/ui/wcm/admin/smoke/AssetsAndFilesActionIT.java
@@ -26,7 +26,6 @@ import org.apache.sling.testing.clients.ClientException;
 import org.apache.sling.testing.clients.util.ResourceUtil;
 import org.apache.sling.testing.clients.util.poller.Polling;
 import org.codehaus.jackson.JsonNode;
-import org.junit.Assert;
 import org.junit.ClassRule;
 import org.junit.Rule;
 import org.junit.Test;
@@ -35,15 +34,16 @@ import org.junit.rules.TestRule;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.IOException;
 import java.io.InputStream;
 import java.util.concurrent.TimeoutException;
 
 /**
  * A set of simple asset tests to verify that asset creating is working.
  */
-public class AssetsActionIT {
+public class AssetsAndFilesActionIT {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(AssetsActionIT.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(AssetsAndFilesActionIT.class);
 
     private static final long TIMEOUT = 3000;
     private static final long RETRY_DELAY = 500;
@@ -65,15 +65,18 @@ public class AssetsActionIT {
     public TempFolderRule damRootFolderRule = new TempFolderRule(cqAuthorPublishClassRule.authorRule, TIMEOUT, RETRY_DELAY);
 
     /**
-     * <ul> <li>Creates a new Asset below /content/dam/qatestrootfolder.</li> <li>Deletes the Asset as user
-     * 'admin'.</li> <li>Verifies that Asset has been correctly deleted</li> </ul>
+     * <ul>
+     *     <li>Creates a new Asset below /content/dam/qatestrootfolder.</li>
+     *     <li>Deletes the Asset as user 'admin'.</li>
+     *     <li>Verifies that Asset has been correctly deleted</li>
+     * </ul>
      *
      * @throws InterruptedException
      * @throws ClientException
      * @throws TimeoutException
      */
     @Test
-    public void testCreateDeleteAssetAsAdmin() throws ClientException, InterruptedException {
+    public void testCreateDeleteAssetAsAdmin() throws ClientException, InterruptedException, TimeoutException {
         CQClient adminAuthor = cqAuthorPublishClassRule.authorRule.getAdminClient(CQClient.class);
         String damRootFolder = damRootFolderRule.getTempFolder();
 
@@ -85,23 +88,70 @@ public class AssetsActionIT {
         String newFileHandle = adminAuthor.uploadAsset(fileName, ASSET_TO_UPLOAD, mimeType, damRootFolder, 200, 201).getSlingPath();
         cleanUpRule.addPath(newFileHandle);
 
-        try {
+        LOGGER.info("Polling for asset creation.");
 
-            LOGGER.info("Polling for file creation.");
+        new Polling(() -> {
+            LOGGER.info("Started poll...");
+            return checkIfAssetExists(adminAuthor, newFileHandle, ResourceUtil.getResourceAsStream(ASSET_TO_UPLOAD), mimeType);
+        }).poll(TIMEOUT, RETRY_DELAY);
 
-            new Polling(() -> {
-                LOGGER.info("Started poll...");
-                return checkIfAssetExists(adminAuthor, newFileHandle, ResourceUtil.getResourceAsStream(ASSET_TO_UPLOAD), mimeType);
-            }).poll(TIMEOUT, RETRY_DELAY);
+        LOGGER.info("Asset was created properly.");
 
-            LOGGER.info("File was created properly.");
-
-        } catch(TimeoutException e) {
-            LOGGER.error("Polling that file was created reached timeout of {}", TIMEOUT);
-        }
     }
 
+    /**
+     * <ul>
+     *     <li>Creates a new file below /content/dam/qatestrootfolder.</li>
+     *     <li>Deletes the file as user 'admin'.</li>
+     *     <li>Verifies that file has been correctly deleted</li>
+     * </ul>
+     *
+     * @throws InterruptedException
+     * @throws ClientException
+     * @throws TimeoutException
+     */
+    @Test
+    public void testCreateDeleteFileAsAdmin() throws ClientException, InterruptedException, TimeoutException {
+        CQClient adminAuthor = cqAuthorPublishClassRule.authorRule.getAdminClient(CQClient.class);
+        String damRootFolder = damRootFolderRule.getTempFolder();
 
+        LOGGER.info("Testing file creation through Admin Console");
+        String fileName = ASSET_TO_UPLOAD.substring(ASSET_TO_UPLOAD.lastIndexOf('/') + 1);
+        String mimeType = "image/jpg";
+
+        LOGGER.info("Creating file..");
+        String newFileHandle = adminAuthor.uploadFileCQStyle(fileName, ASSET_TO_UPLOAD, mimeType, damRootFolder, 200, 201).getSlingPath();
+        cleanUpRule.addPath(newFileHandle);
+
+        LOGGER.info("Polling for file creation.");
+
+        new Polling(() -> {
+            LOGGER.info("Started poll...");
+            return checkIfFileExists(adminAuthor, newFileHandle, ResourceUtil.getResourceAsStream(ASSET_TO_UPLOAD), mimeType);
+        }).poll(TIMEOUT, RETRY_DELAY);
+
+        LOGGER.info("File was created properly.");
+
+    }
+
+    /**
+     * Tests if a Asset exists at the location <tt>path</tt>. It verifies by requesting the json output
+     * of the node and verifying that all nodes and properties are properly created.<br>
+     * <br>
+     * It also makes a binary compare between the original file data and the requested rendition named
+     * <tt>original</tt>.<br>
+     * <br>
+     * NOTE: This method makes no assumptions about the type of the uploaded file (image, pdf, etc)
+     * so it only verifies if the <tt>Metadata</tt> node and <tt>renditions</tt> folder was created.
+     * It does not verify extracted Metadata or check what renditions have been created. The
+     * only rendition verified is the one named <tt>original</tt> through binary compare with the original
+     * file.
+     *
+     * @param client   The client used to request the json for the folder node.
+     * @param path     Path to the asset in question.
+     * @param fileData The file's contents that were uploaded.
+     * @param mimeType file mime type
+     */
     private boolean checkIfAssetExists(CQClient client, String path, InputStream fileData, String mimeType) {
         // Get the root node as JsonNode object
         JsonNode node = null;
@@ -189,6 +239,89 @@ public class AssetsActionIT {
             }
         } catch (Exception e) {
             LOGGER.info("Binary compare of files failed!");
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * Tests if a File exists at the location <tt>path</tt>. It verifies by requesting the json output
+     * of the node and verifying that all nodes and properties are properly created.<br>
+     * <br>
+     * It also makes a binary compare between the original file data and the requested file.
+     * <br>
+     *
+     * @param client   The client used to request the json for the folder node.
+     * @param path     Path to the file in question.
+     * @param fileData The file's contents that were uploaded.
+     * @param mimeType file mime type
+     */
+    private boolean checkIfFileExists(final CQClient client, String path, InputStream fileData, String mimeType) {
+
+        // Get the root node as JsonNode object
+        JsonNode node = null;
+        try {
+            node = client.adaptTo(JsonClient.class).doGetJson(path, -1);
+        } catch (ClientException e) {
+            LOGGER.error("Request for " + path + " failed");
+            return false;
+        }
+
+        // check if jcr:primaryType is set to sling:OrderedFolder
+        if(!node.get("jcr:primaryType").getValueAsText().equals("sling:OrderedFolder")) {
+            LOGGER.info("jcr:primaryType of folder node " + path + " is not set to sling:OrderedFolder!");
+            return false;
+        }
+
+        // check if file node exists
+        if(node.path("file").isMissingNode()) {
+            LOGGER.info("No file node found below " + path + "!");
+            return false;
+        }
+
+
+        // Get the file node
+        node = node.path("file");
+        path += "/file";
+
+        // check if jcr:primaryType is set to nt:file
+        if(!node.get("jcr:primaryType").getTextValue().equals("nt:file")) {
+            LOGGER.info("jcr:primaryType of file node below " + path + " is not set to nt:file!");
+            return false;
+        }
+
+        // check if jcr:content node exists
+        if(node.path("jcr:content").isMissingNode()) {
+            LOGGER.info("No jcr:content node found below " + path + "!");
+            return false;
+        }
+
+        // Get the jcr:content node
+        node = node.path("jcr:content");
+        path += "/jcr:content";
+
+        // check if jcr:mimeType is set correctly
+        if(!node.get("jcr:mimeType").getTextValue().equals(mimeType)) {
+            LOGGER.info("jcr:mimeType is not set to " + mimeType + ". Actual: " + node.get("jcr:mimeType").getTextValue());
+            return false;
+        }
+
+        // check if jcr:primaryType is set to nt:resource
+        if(!node.get("jcr:primaryType").getTextValue().equals("nt:resource")) {
+            LOGGER.info("jcr:primaryType of jcr:content node below " + path + " is not set to nt:resource!");
+            return false;
+        }
+
+
+        try {
+            InputStream in = client.doStreamGet(path, null, null).getEntity().getContent();
+            if(!TestUtil.binaryCompare(fileData, in)) {
+                LOGGER.info("The original file and the requested file are not the same");
+                return false;
+            }
+        } catch (IOException | ClientException e) {
+            LOGGER.error(e.getMessage());
             return false;
         }
 
