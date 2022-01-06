9220faa7f1a00e404bac9d029149270fce5e97a7
Abhinav Saraswat <51747444+absarasw@users.noreply.github.com>
2022-01-06 15:16:15 +0530

    Adding AssumptionViolatedException when Author is unauthorised to create page or Sling response is modified (#43)
    * Adding AssumptionViolatedException when Author is unauthorised to create page or Sling response is modified
    
    * Adding user name in AssumptionViolatedException
    
    * Adding assert in case the created page path can not be retrieved from response.
    
    Co-authored-by: Abhinav Saraswat <abhinavsaraswat@Abhinavs-MacBook-Pro.local>

--- smoke/src/main/java/com/adobe/cq/cloud/testing/it/smoke/CreatePageAsAuthorUserIT.java
+++ smoke/src/main/java/com/adobe/cq/cloud/testing/it/smoke/CreatePageAsAuthorUserIT.java
@@ -25,6 +25,7 @@ import org.apache.http.HttpStatus;
 import org.apache.sling.testing.clients.ClientException;
 import org.apache.sling.testing.clients.SlingHttpResponse;
 import org.junit.Assert;
+import org.junit.AssumptionViolatedException;
 import org.junit.ClassRule;
 import org.junit.Ignore;
 import org.junit.Rule;
@@ -37,6 +38,8 @@ import org.slf4j.LoggerFactory;
 import java.util.UUID;
 
 import static java.util.concurrent.TimeUnit.MINUTES;
+import static org.apache.http.HttpStatus.SC_UNAUTHORIZED;
+import static org.junit.Assert.assertFalse;
 
 public class CreatePageAsAuthorUserIT {
 
@@ -71,11 +74,24 @@ public class CreatePageAsAuthorUserIT {
     @Test
     public void testCreatePageAsAuthor() throws InterruptedException, ClientException {
         String pageName = "testpage_" +  UUID.randomUUID();
-        String pagePath = temporaryPage.getParentPath() + "/" + pageName;
+        String pagePathExpected = temporaryPage.getParentPath() + "/" + pageName;
+        String pagePath = pagePathExpected;
         try {
             SlingHttpResponse response = userRule.getClient().createPageWithRetry(pageName, "Page created by CreatePageAsAuthorUserIT",
                     temporaryPage.getParentPath(), "", MINUTES.toMillis(1), 500, HttpStatus.SC_OK);
+            if (null != response && response.getStatusLine().getStatusCode() == SC_UNAUTHORIZED) {
+                throw new AssumptionViolatedException("Author User " + userRule.getClient().getUser() + " not authorized to create page. Skipping...");
+            }
             pagePath = response.getSlingLocation();
+            if(pagePath == null || pagePath.isEmpty()) {
+                pagePath = response.getSlingPath();
+            }
+            String expectedResponseLike = "<tr>\n <td>Location</td>\n" +
+                    " <td><a href=\""  + pagePathExpected +"\" id=\"Location\">" + pagePathExpected +"</a></td>\n</tr>\n.\n.\n.\n" +
+                    "<tr>\n <td>Path</td>\n" +
+                    " <td><div id=\"Path\">" + pagePathExpected + "</div></td>\n" +
+                    "</tr>";
+            assertFalse("Not able to get created page path from sling response. Expected response like " + expectedResponseLike, pagePath == null || pagePath.isEmpty());
             LOG.info("Created page at {}", pagePath);
 
             // This shows that it exists for the author user
