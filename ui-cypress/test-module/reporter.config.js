51438f670a4b894c1317b2c4363be2bda68d5881
Ramon Bisswanger <ramon@bisswanger.de>
2023-05-25 14:04:14 +0200

    CQ-4353361: junit reporter did overwrite the XML reports during execution (#73)

--- ui-cypress/test-module/reporter.config.js
+++ ui-cypress/test-module/reporter.config.js
@@ -19,6 +19,6 @@ const reportsPath = process.env.REPORTS_PATH || 'cypress/results'
 module.exports = {
   reporterEnabled: 'spec, mocha-junit-reporter',
   mochaJunitReporterReporterOptions: {
-    mochaFile: `${reportsPath}/output.xml`
+    mochaFile: `${reportsPath}/output.[hash].xml`
   },
 }
